4833e3e4065f5b26b079a1bcd69aa44de2ca3ec6
Tinita <tino4ka.87@mail.ru>
2024-05-21 21:51:46 +0300

    Initial commit

---
+++ jest-puppeteer.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    launch: {
+        headless: false,
+        devtools: true,
+        defaultViewport: null,
+        args: ['--start-maximized'] //— используем максимальный размер окна браузера
+      },
+  };
